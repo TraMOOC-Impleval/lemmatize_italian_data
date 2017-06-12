a910e0f10442312e4b2aac06bd7f075d44ff185e
hdvos <hdvos93@gmail.com>
2017-06-12 13:23:09 +0200

    Add files via upload

--- master/pymorphit.py
+++ master/pymorphit.py
@@ -1,69 +1,93 @@
-#coding: utf-8
+# coding: utf-8
 
 # by G. De Gasperis and I. Grappasonno, UnivAQ http://www.univaq.it
+# adapted by: H. de Vos
 
+#package for debug
+import time
+
+#Packages for technical support
 import re
 import codecs
 import json
 import os
+import sys
 
+#Packages for linguistic support
 from string import punctuation
 
+# Globals
 UNKOWN = u'?'
+
+# Initialisation funcs --------------------------------------
 
 def catChoice():
     global catS
     catL = list(catS)
     ci = 1
-    for c in catL:
-        print ci, c
+    for i, c in enumerate(catL):
+        print(i+1, c)
         ci += 1
-    a = input('Quale? :')
-    if (a>0) and (a<=len(catL)):
+        ci += 1
+    a = int(input('Quale? :'))
+    if (a > 0) and (a <= len(catL)):
         return catL[a]
     else:
         return 'J:j'
 
+
 def addCatTree(catK, catV):
     global catTree
-    if not catTree.has_key(catK):
+    if not catK in catTree.keys():
         catTree[catK] = set([])
-    catTree[catK]= catTree[catK].union(catTree[catK], set(catV))
+    catTree[catK] = catTree[catK].union(catTree[catK], set(catV))
     pass
+
 
 def makeTupleList(lx):
     out = []
     for e in lx:
-        if type(e)==type((0,0)):
+        if type(e) == type((0, 0)):
             out.append(e)
-        elif type(e)==type([]):
+        elif type(e) == type([]):
             out += makeTupleList(e)
         else:
-            print '?????'
+            print('?????')
     return out
 
+# Tokenization funcs --------------------------------------------------------
+
 def tokenize(line):
-    scanner=re.Scanner([
-      (ur"[0-9]+",  lambda scanner,token:("DET-NUM", token)),
-      (ur"[A-Z]*[a-z]+[àèéìòù]*", lambda scanner,token:("LESSEMA", token)),
-      (ur"[!.?]+",  lambda scanner,token:("PUNT_FIN", token)),
-      (ur"[,;:]+",  lambda scanner,token:("PUNT", token)),
-      (ur"\s+", None), # None == skip token.
+    scanner = re.Scanner([
+        (r"[0-9]+", lambda scanner, token: ("DET-NUM", token)),
+        (r"[A-Z]*[a-z]+[àèéìòù]*", lambda scanner, token: ("LESSEMA", token)),
+        (r"[!.?]+", lambda scanner, token: ("PUNT_FIN", token)),
+        (r"[,;:]+", lambda scanner, token: ("PUNT", token)),
+        (r"\s+", None),  # None == skip token.
     ])
     out0 = scanner.scan(line)
     return makeTupleList(out0)
 
+
+# Lemmatization funcs --------------------------------------------------------
+
 def RomanTranslate(s):
+    '''
+
+    :param s: <str> Roman Number
+    :return: <int> Decimal equivalent of the roman number
+    '''
     string = s.upper()
-    values = {"I":1, "V":5, "X":10, "L":50, "C":100, "M":1000}
+    values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "M": 1000}
     try:
         return sum(map(lambda x: values[x], string))
     except:
         return ''
 
+
 def isNumber(token):
     out = False
-    if len(token) >0:
+    if len(token) > 0:
         try:
             f = float(token)
             out = True
@@ -73,40 +97,49 @@ def isNumber(token):
             pass
     return out
 
+'''
+I am not sure what this part does.
+However, it works perfectly fine without. So I commented it out.
+
 def log(s):
     if type(s) in [type(u''), type('')]:
         return s.encode('utf-8')
     else:
         out = ''
         for z in s:
-            out += '\t' + log(z)
+            out += '\t' + log(str(z).encode('utf-8'))
         return out
+'''
 
 def learnLemma(lemmaPrec, lessema, succ):
     global dMorphit, dRules
-    ltupla = unicode((lemmaPrec[1], lessema, succ[1]))
-    if dRules.has_key(ltupla):
+    ltupla = str((lemmaPrec[1], lessema, succ[1]))
+    if ltupla in dRules.keys():
         c = dRules[ltupla]
-        print 'regola:',log(ltupla),log(c)
+        print ('regola:', ltupla, c)
         return c
+
     else:
-        print 'NUOVA REGOLA!'
-        print 'Contesto: [..',log(lemmaPrec[0]), log(lessema), log(succ[0]),'..]'
-        print '\t\t', '<'+log(lemmaPrec[1])+'>', log(lessema), '<'+log(succ[1])+'>'
-        print 0, 'Save and Exit'
+        print('NUOVA REGOLA!')
+        print ('Contesto: [..', lemmaPrec[0], lessema, succ[0],'..]')
+        print ('\t\t', '<'+lemmaPrec[1]+'>', lessema, '<'+succ[1]+'>')
+        print(0, 'Save and Exit')
         lemmi = dMorphit[lessema]
-        for m in range(1,len(lemmi)+1):
-            print m, log(lemmi[m-1])
-        print -1,'Categoria generica'
-        a = input('Quale? :')
-        if a>0:
-            out = (lemmi[a-1][0], lemmi[a-1][1])
+        for m in range(1, len(lemmi) + 1):
+            print (m, lemmi[m-1])
+            pass
+        print(-1, 'Categoria generica')
+        a = int(input('Quale? :'))
+
+        if a > 0:
+            out = (lemmi[a - 1][0], lemmi[a - 1][1])
             dRules[ltupla] = out
             return out
         else:
-            if a==-1:
+            if a == -1:
                 return (lessema, UNKOWN)
             return ''
+
 
 def makeLemma(lemmaPrec, lessema, succ):
     out = learnLemma(lemmaPrec, lessema, succ)
@@ -115,17 +148,21 @@ def makeLemma(lemmaPrec, lessema, succ):
         exit(0)
     return out
 
+
 def hasLemma(lessema):
+
     global dMorphit
-    return dMorphit.has_key(lessema)
+    return lessema in dMorphit.keys()
+
 
 def getLemma(lemmaPrec, lessema, succ):
-    out = u''
+
+    out = u''                  #<-- redundant.
     lemmi = dMorphit[lessema]
     out = lemmi[0]
     if len(lemmi) > 1:
         # ambiguità
-        if lemmaPrec!=UNKOWN:
+        if lemmaPrec != UNKOWN:
             succLemma = lemmatize(UNKOWN, succ, UNKOWN)
             out = makeLemma(lemmaPrec, lessema, succLemma)  # usa regole ed euristiche per determinare il lemma
         else:
@@ -134,10 +171,20 @@ def getLemma(lemmaPrec, lessema, succ):
 
 
 def lemmatize(lemmaPrec, lessema, succ):
+    '''
+    Lemmatize the target word.
+    :param lemmaPrec: <tuple> (lemma, POS-tag) lemmatized word preceding to target word
+    :param lessema: <tuple> (type ('LESSEMA (lexeme) or PUNT (Punctuation)), target word)
+    :param succ: <>unlemmatized word succeeding the target word.
+    :return: <tuple> (lemma, POS-tag)
+    '''
+
     out = u''
-    print lessema
-    print log(lessema), '...',
-    if len(lessema)>0:
+    if type(lessema) == tuple:
+        lessema = lessema[1]
+
+    print (lessema, '...',)
+    if len(lessema) > 0:
         if hasLemma(lessema):
             out = getLemma(lemmaPrec, lessema, succ)
         elif hasLemma(lessema.lower()):
@@ -148,70 +195,100 @@ def lemmatize(lemmaPrec, lessema, succ):
             out = (u'X', u'DET-NUM')
         else:
             out = (lessema, UNKOWN)
-    if lemmaPrec!=UNKOWN:
-        print '\t-->\t', log(out)
+    if lemmaPrec != UNKOWN:
+        print ('\t-->\t', out)
     return out
 
-#------------ MAIN -----------------
-tl = tokenize(u'Il turista che andò al mare, e mai più ci Tornerà.')
-print tl
-pass
-myPuntuaction = punctuation
-myPuntuaction = myPuntuaction.replace('-','')
-r = re.compile(r'[\s{}]+'.format(re.escape(myPuntuaction)))
 
-dMorphit = {}
-dRules = {}
-catTree = {}
+# ------------ MAIN -----------------
+if __name__ == '__main__':
 
-DRULES_FILE = 'drules.json'
-if os.path.isfile(DRULES_FILE):
-    dRules = json.loads(open(DRULES_FILE,'r').read())
+    #INFILE = sys.argv[1]  #<-- uncomment after debugging
+    INFILE = 'collodi_pinocchio_utf8.txt'          #<-- comment after debugging
 
+    tl = tokenize(u'Il turista che andò al mare, e mai più ci Tornerà.')
+    print(tl)
+    pass
+    myPuntuaction = punctuation
+    myPuntuaction = myPuntuaction.replace('-', '')
+    r = re.compile(r'[\s{}]+'.format(re.escape(myPuntuaction)))
 
-# DMORPHIT_FILE = 'dmorphit.json'
-# if os.path.isfile(DMORPHIT_FILE):
-#     dMorphit = json.loads(open(DMORPHIT_FILE,'r').read())
-# else:
-fm =  codecs.open('morph-it_048_utf8.txt','r',encoding='utf-8')
+    dMorphit = {}
+    dRules = {}
+    catTree = {}
 
-for line in fm:
-    lst = re.split(' |\t', line.strip())
-    cat = lst[2]
-    if cat.find('-')>0:
+    DRULES_FILE = 'drules.json'
+    if os.path.isfile(DRULES_FILE):
+        dRules = json.loads(open(DRULES_FILE, 'r').read())
+
+    # DMORPHIT_FILE = 'dmorphit.json'
+    # if os.path.isfile(DMORPHIT_FILE):
+    #     dMorphit = json.loads(open(DMORPHIT_FILE,'r').read())
+    # else:
+
+    fm = codecs.open('morph-it_048_utf8.txt', 'r', encoding='utf-8')
+
+    for line in fm:
+        lst = re.split(' |\t', line.strip())
+        cat = lst[2]
+
+        if cat.find('-') > 0:
             catL2 = cat.split('-')
             addCatTree(catL2[0], catL2[1:])
-    elif cat.find(':')>0:
-        [catL1l, catL1r] = cat.split(':')
-        if catL1l.find('-')>0:
-            [catL2l, catL2r] = catL1l.split('-')
-            addCatTree(catL2l, catL2r)
+        elif cat.find(':') > 0:
+            [catL1l, catL1r] = cat.split(':')
+            if catL1l.find('-') > 0:
+                [catL2l, catL2r] = catL1l.split('-')
+                addCatTree(catL2l, catL2r)
+            else:
+                addCatTree(catL1l, '')
         else:
-            addCatTree(catL1l,'')
-    else:
-        addCatTree(cat,'')
-    try:
-        if dMorphit.has_key(lst[0]):
-            dMorphit[lst[0]] += [(lst[1], lst[2])]
-        else:
-            dMorphit[lst[0]] = [(lst[1], lst[2])]
-    except:
-        pass
-#open(DMORPHIT_FILE,'w').write(json.dumps(dMorphit))
+            addCatTree(cat, '')
+        try:
+            if lst[0] in dMorphit.keys():
+                dMorphit[lst[0]] += [(lst[1], lst[2])]
+            else:
+                dMorphit[lst[0]] = [(lst[1], lst[2])]
+        except:
+            pass
+    # open(DMORPHIT_FILE,'w').write(json.dumps(dMorphit))
 
-with codecs.open('collodi_pinocchio_utf8.txt','r','utf-8') as fc:
-    lemmaPrec = u'[]'
-    lemmabile = lemmaPrec
-    succ = u''
-    for line in fc:
-        tl = tokenize(line.strip())
-        widx = 0
-        for t in tl:
-            if t[0]=='LESSEMA':
-                succ = '[]'
-                if widx < len(tl) - 1:
-                    succ = tl[widx + 1]
-                if len(lemmabile)>0:
-                    lemmaPrec = lemmabile
-                lemmabile = lemmatize(lemmaPrec, t[1], succ)
-                widx += 1
+    print('initiation done...', flush=True)
+    time.sleep(5)
+
+    with codecs.open(INFILE, 'r', 'utf-8') as fc:
+        lemmaPrec = u'[]'
+        lemmabile = lemmaPrec
+        succ = u''
+
+        for line in fc:
+
+            tl = tokenize(line.strip())
+
+            widx = 0
+
+            for widx2, t in enumerate(tl):
+
+                if t[0] == 'LESSEMA':
+                    succ = '[]'
+                    if widx < len(tl) - 1:
+                        succ = tl[widx2 + 1]
+
+                    if len(lemmabile) > 0:
+                        lemmaPrec = lemmabile
+
+
+                    print('-------------------')
+                    print(tl)
+
+                    print('lemma:', lemmabile)
+
+                    print('lemmaprec:', lemmaPrec)
+                    print('t1:', t[1])
+                    print('succ:', succ)
+
+
+                    lemmabile = lemmatize(lemmaPrec, t[1], succ)
+                    widx += 1
+
+                    print('-------------------')
